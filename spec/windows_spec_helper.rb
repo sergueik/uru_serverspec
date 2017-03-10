370fc9270438fe3fa853beca911470b6db0a71b8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-03-10 17:47:31 -0500

    intermediate revision - added comment

--- spec/windows_spec_helper.rb
+++ spec/windows_spec_helper.rb
@@ -1,7 +1,7 @@
 require 'serverspec'
 
 set :backend, :cmd
-
+# NOTE: the following is only needed for vagrant-serverspec launch
 RSpec.configure do |config|
   config.filter_gems_from_backtrace 'vagrant', 'vagrant-serverspec'
 end
