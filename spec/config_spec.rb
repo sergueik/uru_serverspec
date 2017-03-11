861b67486b5917cdaed67b2d8c0d3f2e3b7d350e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-03-11 12:08:39 -0500

    intermediate revision - refactoring examples

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -7,67 +7,97 @@ require 'csv'
 
 uru_home = 'c:/uru'
 user_home = ENV.has_key?('VAGRANT_EXECUTABLE') ? 'c:/users/vagrant' : ( 'c:/users/' + ENV['USER'] )
+config_file = "#{uru_home}/spec/config/config.yaml"
+def contents(file)
+  command("get-content #{file} -Encoding ASCII").stdout
+end
 
-context 'Configuration',:if => ENV.has_key?('URU_INVOKER')  do
-  context 'Find the file' do
-    describe file("#{uru_home}/spec/config/config.yaml") do
-      it { should be_file }
-      {
-        'key1' => 'value1',
-        'key2' => 'value2',
-      }.each do |key, value|
-        it { should contain /#{key}: #{value}/}
-      end
+context 'Read the file contents with command (e.g. remotely)' do
+  {
+    'key1' => 'value1',
+    'key2' => 'value2',  # replace with an invalid value to see the output
+  }.each do |key, value|
+    it do
+      expect(contents(config_file)).to match /#{key}: #{value}/
     end
   end
+end
 
-  context 'Read the file contents with command' do
-    def contents(file)
-      command("get-content #{file}").stdout
-    end
+context 'Read the file', :if => ENV.has_key?('URU_INVOKER') do
+  describe file(config_file) do
+    it { should be_file }
     {
       'key1' => 'value1',
       'key2' => 'value2',
-      }.each do |key, value|
-      it do
-        expect(contents("#{uru_home}/spec/config/config.yaml")).to match /#{key}: #{value}/
-      end
+    }.each do |key, value|
+      it { should contain /#{key}: #{value}/}
     end
   end
+end
 
+context 'Confirm valid YAML', :if => ENV.has_key?('URU_INVOKER') do
+   if ENV.has_key?('URU_INVOKER')
+		# NOTE: saving example config.yaml with Windows unicode encoding may cause
+    # ASCII incompatible encoding needs binmode (ArgumentError)
+    # TODO: path
+    parameters = YAML.load_file(File.join(__dir__, '../config/config.yaml'))
+    it do
+      expect(parameters['key1']).to eq('value1')
+    end
+  else
+    STDERR.puts 'Skipped'
+  end
+end
+
+# NOTE: this does not work
+context 'Confirm valid YAML', :if => ENV.has_key?('URU_INVOKER') do
+  data = nil
+  parameters = {} 
+  begin
+    data = contents(config_file)
+    STDERR.puts data
+  rescue => e
+    STDERR.puts e.to_s 
+    # undefined method `metadata' for nil:NilClass
+    # see https://github.com/sergueik/puppetmaster_vagrant/blob/master/spec/type/command.rb for solution
+  end
+  if data
+    parameters = YAML.load(data)
+  end
+  it do
+    expect(parameters.has_key?('key1')).to be_truthy
+  end
+  it do
+    expect(parameters['key1']).to eq('value1')
+  end
+end
+
+# NOTE: this does not work
   context 'Confirm it is a valid YAML', :if => ENV.has_key?('URU_INVOKER') do
     load_status = nil
-    filename ="#{uru_home}/spec/config/config.yaml"
-    describe command("get-content -LiteralPath '#{filename}' -Encoding ASCII") do
+    describe command("get-content -LiteralPath '#{config_file}' -Encoding ASCII") do
       {
         'key1' => 'value1',
         'key2' => 'value2',
       }.each do |key, value|
-        its(:stdout) { should contain /#{key}: #{value}/}
-      end
-    end
-    command_result = Specinfra::Runner::run_command( <<-EOF
-      get-content -LiteralPath '#{filename}' -Encoding ASCII
-    EOF
-    ).stdout.gsub(/\r/,'')
-    puts 'Command output: ' + command_result
-    # NOTE: this is failing
-    begin
-      YAML.load(command_result)
-      load_status = true
-    rescue => e
-      puts 'Exceptions: ' + e.to_s
-      load_status = false
-    end
-    puts 'load status: ' + load_status.to_s
-    describe(load_status) do
-      it { should be true }
+      its(:stdout) { should contain /#{key}: #{value}/}
     end
   end
-  context 'Confirm able to load ' do
-    if ENV.has_key?('URU_INVOKER')
-      parameters = YAML.load_file(File.join(__dir__, '../config/config.yaml'))
-      parameter = parameters['key1']
-    end
+  command_result = Specinfra::Runner::run_command( <<-EOF
+    get-content -LiteralPath '#{config_file}' -Encoding ASCII
+  EOF
+  ).stdout.gsub(/\r/,'')
+  puts 'Command output: ' + command_result
+  # NOTE: this is failing
+  begin
+    YAML.load(command_result)
+    load_status = true
+  rescue => e
+    puts 'Exceptions: ' + e.to_s
+    load_status = false
+  end
+  puts 'load status: ' + load_status.to_s
+  describe(load_status) do
+    it { should be true }
   end
 end
