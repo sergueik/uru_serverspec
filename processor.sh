a2d8e0adda0aa8cd126fb67475943cee3e44fa80
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-19 11:53:43 -0400

    intermediate revision - fixed typo and modifid colored report in shell serverspec run processor

--- processor.sh
+++ processor.sh
@@ -3,12 +3,22 @@
 RESULTS_FILENAME='result_.json'
 RESULTS_DIRECTORY='results'
 
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+RED='\033[0;31m'
+LIGHT_GREEN='\033[1;32m'
+BROWN='\033[0;33m'
+NC='\033[0m' # No Color
+
 # https://stedolan.github.io/jq/manual/
 
-which jq 1 > /dev/null 2 >& 1
+which jq 1>/dev/null 2>& 1
 if [ $? == 0 ] ; then
-  pusd $RESULTS_DIRECTORY
-  jq ' .examples[] | select(.status != "passed")| .description,.status' $RESULTS_FILENAME
-  jq '.summary_line' $RESULTS_FILENAME
-  popd
+  >/dev/null pushd $RESULTS_DIRECTORY
+  printf "${RED}"
+  jq -M ' .examples[] | select(.status != "passed")| .description,.status' $RESULTS_FILENAME
+  printf "${NC}"
+  printf "${LIGHT_GREEN}Summary: ${BROWN}"
+  jq -M '.summary_line' $RESULTS_FILENAME
+  printf "${NC}"
+  >/dev/null popd
 fi
