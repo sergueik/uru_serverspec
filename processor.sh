7fc2e89ac8b8bbb5f91e1959eca092729e78a3fc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-06-19 15:23:04 -0400

    intermediate revision - fixed typo and modifid colored report in shell serverspec run processor, updated ruby report processor

--- processor.sh
+++ processor.sh
@@ -3,11 +3,21 @@
 RESULTS_FILENAME='result_.json'
 RESULTS_DIRECTORY='results'
 
-# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-RED='\033[0;31m'
-LIGHT_GREEN='\033[1;32m'
-BROWN='\033[0;33m'
-NC='\033[0m' # No Color
+# https://stackoverflow.com/questions/911168/how-to-detect-if-my-shell-script-is-running-through-a-pipe
+if [ -t 1 ] ; then
+  # terminal
+  # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+  RED='\033[0;31m'
+  LIGHT_GREEN='\033[1;32m'
+  BROWN='\033[0;33m'
+  NC='\033[0m' # no color
+else
+  # not a terminal
+  RED=''
+  LIGHT_GREEN=''
+  BROWN=''
+  NC=''
+fi
 
 # https://stedolan.github.io/jq/manual/
 
@@ -15,7 +25,9 @@ which jq 1>/dev/null 2>& 1
 if [ $? == 0 ] ; then
   >/dev/null pushd $RESULTS_DIRECTORY
   printf "${RED}"
-  jq -M ' .examples[] | select(.status != "passed")| .description,.status' $RESULTS_FILENAME
+  # https://github.com/stedolan/jq/issues/785
+  # https://stackoverflow.com/questions/39139107/how-to-format-a-json-string-as-a-table-using-jq
+  jq -M ' .examples[] | select(.status != "passed")| "\(.full_description)\n"' $RESULTS_FILENAME | awk '{print $0}' | sed 's/Command.*$//'
   printf "${NC}"
   printf "${LIGHT_GREEN}Summary: ${BROWN}"
   jq -M '.summary_line' $RESULTS_FILENAME
