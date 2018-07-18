0c058a67f34ac220e97b8ecdc271eefc216c9b53
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-07-18 12:08:59 -0400

    intermediate revision - cosmetic changes and argument processing

--- processor.sh
+++ processor.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-WORKDIR='/uru'
-RESULTS_FILENAME='result_.json'
-RESULTS_DIRECTORY='results'
+WORKDIR=${1:-/uru}
+RESULTS_BASENAME=${2:-result}
+
+RESULTS_FILENAME="${RESULTS_BASENAME}_.json"
+RESULTS_DIRECTORY="${RESULTS_BASENAME}s"
 
 # https://stackoverflow.com/questions/911168/how-to-detect-if-my-shell-script-is-running-through-a-pipe
 if [ -t 1 ] ; then
   # terminal
   # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
   RED='\033[0;31m'
+  LIGHT_RED='\033[1;31m'
   LIGHT_GREEN='\033[1;32m'
   BROWN='\033[0;33m'
   NC='\033[0m' # no color
@@ -16,6 +19,7 @@ else
   # not a terminal
   RED=''
   LIGHT_GREEN=''
+  LIGHT_RED=''
   BROWN=''
   NC=''
 fi
@@ -26,13 +30,27 @@ which jq 1>/dev/null 2>& 1
 if [ $? != 0 ] ; then
   exit 1
 fi
+if [ ! -d $WORKDIR  ] ; then
+  echo "Invalid work directory: \"${WORKDIR}\""
+  exit 1
+fi
 >/dev/null pushd $WORKDIR
+if [ ! -d $RESULTS_DIRECTORY  ] ; then
+  echo "Invalid results directory: \"${WORKDIR}/${RESULTS_DIRECTORY}\""
+  exit 1
+fi
 >/dev/null pushd $RESULTS_DIRECTORY
-printf "${RED}"
 # https://github.com/stedolan/jq/issues/785
 # https://stackoverflow.com/questions/39139107/how-to-format-a-json-string-as-a-table-using-jq
-jq -M ' .examples[] | select(.status != "passed")| "\(.full_description)\n"' $RESULTS_FILENAME | awk '{print $0}' | sed 's/Command.*$//'
-printf "${NC}"
+jq -M '.examples[] | select(.status != "passed")| (.status)' $RESULTS_FILENAME | grep -q 'failed'
+if [ $? != 0 ] ; then
+  printf "${LIGHT_GREEN}No failed tests.\n${NC}"
+else
+  printf "${LIGHT_RED}Failed tests:\n${NC}"
+  printf "${RED}"
+  jq -M ' .examples[] | select(.status == "failed")| "\(.full_description)\n"' $RESULTS_FILENAME | awk '{print $0}' | sed 's/Command.*$//'
+  printf "${NC}"
+fi
 printf "${LIGHT_GREEN}Summary: ${BROWN}"
 jq -M '.summary_line' $RESULTS_FILENAME
 printf "${NC}"
