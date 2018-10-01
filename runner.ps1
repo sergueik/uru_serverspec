734d732f7a603fb297eb0511b9ef087f7258b514
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-10-01 16:48:43 -0400

    intermediate revision - added per_user support

--- runner.ps1
+++ runner.ps1
@@ -1,32 +1,41 @@
 param (
-  [switch]$debug
+  [switch]$debug,
+  [switch]$per_user
 )
 
+$per_user_install = [bool]$PSBoundParameters['per_user'].IsPresent
+if ($debug){
+  $debugpreference_saved = $debugpreference
+  $debugpreference = 'continue'
+}
+if (($DEFAULT_URU_HOME -eq $null) -or ($DEFAULT_URU_HOME -eq '') ) {
+  if ($per_user_install) {
+    if ([Environment]::GetFolderPath('Personal') -match '^\\\\' ) {
+      $DEFAULT_URU_HOME = "C:\users\${env:USERNAME}\Appdata\Roaming\uru"
+    } else {
+      $DEFAULT_URU_HOME = ('{0}\Appdata\Roaming\uru' -f ([Environment]::GetFolderPath('Personal')  -replace '\\Documents', '' ))
+    }
+  } else {
+    $DEFAULT_URU_HOME = 'C:\uru'
+  }
+}
 
-# $local:URU_HOME='<%= @uru_home -%>'
+# $script:URU_HOME='<%= @uru_home -%>'
 # Note: the URU_HOME environment should not be changed and exported
 # otherwise leading to the error
 # ---> No rubies registered with uru
-#
-if (($local:URU_HOME -eq $null) -or ($local:URU_HOME -eq '') ) {
-  $local:URU_HOME = 'C:\uru'
-}
 
+$script:URU_HOME = $DEFAULT_URU_HOME
 $env:PATH = "${env:PATH};${URU_HOME}"
 $env:URU_INVOKER = 'powershell'
-
-# $GEM_VERSION = '2.1.0'
-# $RAKE_VERSION = '10.1.0'
-# $RUBY_VERSION = '2.1.7'
-# $RUBY_VERSION_LONG = '2.1.7-p400'
 
 $GEM_VERSION = '2.3.0'
 $RAKE_VERSION = '10.4.2'
 $RUBY_VERSION = '2.3.3'
 $RUBY_VERSION_LONG = '2.3.3p222'
 $RUBY_TAG_LABEL = $RUBY_VERSION_LONG -replace '[\-\.]', ''
-$URU_RUNNER = "${local:URU_HOME}\uru_rt.exe"
-$RESULTS_PATH = "${local:URU_HOME}\results"
+$URU_RUNNER = "${script:URU_HOME}\uru_rt.exe"
+$RESULTS_PATH = "${script:URU_HOME}\results"
 
 $USERPROFILE = $HOME
 # Cannot overwrite variable HOME because it is read-only or constant.
@@ -39,7 +48,13 @@ if ($USERPROFILE -eq '') {
   # https://msdn.microsoft.com/en-us/library/windows/desktop/bb774096%28v=vs.85%29.aspx
   $ssfPROFILE = 0x28
   $USERPROFILE = (New-Object -ComObject Shell.Application).Namespace($ssfPROFILE).Self.Path
+  if ($USERPROFILE -match '^\\\\' ) {
+    $USERPROFILE= "C:\users\${env:USERNAME}"
+  }
   write-debug "USERPROFILE='${USERPROFILE}'"
+}
+if ($debug){
+  get-variable -include @('URU_HOME','URU_RUNNER', 'USERPROFILE', 'RUBY_VERSION', 'DEFAULT_URU_HOME', 'RESULTS_PATH') | format-list
 }
 if (-not (test-path "${USERPROFILE}\.uru")) {
   mkdir "${USERPROFILE}\.uru" -erroraction silentlycontinue
@@ -53,7 +68,7 @@ if (-not (test-path "${USERPROFILE}\.uru")) {
       "ID": "${RUBY_VERSION_LONG}",
       "TagLabel": "${RUBY_TAG_LABEL}",
       "Exe": "ruby",
-      "Home": "$("${local:URU_HOME}\ruby\bin" -replace '\\', '\\')",
+      "Home": "$("${script:URU_HOME}\ruby\bin" -replace '\\', '\\')",
       "GemHome": "",
       "Description": "ruby $RUBY_VERSION_LONG (2015-08-18 revision 51632) [x64-mingw32]"
     }
@@ -82,7 +97,10 @@ popd
 
 # extract summary_line
 # NOTE: convertFrom-json requires Powershell 3.
-$report = get-content -path "${RESULTS_PATH}\result_.json" | convertfrom-json
+$report = get-content -path "${RESULTS_PATH}\result.json" | convertfrom-json
 write-output ($report.'summary_line')
 
-$local:URU_HOME = $null
+$script:URU_HOME = $null
+if ($debug){
+  $debugpreference = $debugpreference_saved
+}
