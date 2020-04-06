6ef2717a3633dd165c9d642833009c5aca3e1d78
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-04-06 17:14:40 -0400

    intermediate revision - fixed the prefix length computation, added defaults for real job

--- utils/package_two.ps1
+++ utils/package_two.ps1
@@ -1,29 +1,35 @@
 param(
-  [String] $name = 'test',
-  [String] $appDir = 'c:\temp',
-  [String] $resourceDir = 'C:\Program Files\7-Zip',
-  [String] $basedir = 'program files',
-  [Switch] $debug
+	[String] $name = 'uru',
+	[String] $appDir = 'c:\uru\results',
+	[String] $resourceDir = 'C:\Program Files\7-Zip',
+	[String] $basedir = 'C:\program files',
+	# [String] $appDir = 'c:\uru',
+	# [String] $resourceDir = "${env:USERPROFILE}\.gem",
+	# [String] $basedir = $env:USERPROFILE,
+	[Switch] $debug
 )
-# for uru packaging together the binary, system-wide gems and local user gems
-# . .\package_two.ps1 -name uru -appdir c:\uru -resourceDir C:\Users\sergueik\.gem -basedir C:\Users\sergueik
-# NOTE: quite slow! - need to change adding file by file to adding dirs
 
+# for uru packaging together the binary, system-wide gems and local user gems call 
+# . .\package_two.ps1 -debug  -appDir c:\uru -resourceDir "${env:USERPROFILE}\.gem" -basedir $env:USERPROFILE
+# NOTE: quite slow! - need to change adding file by file to adding dirs.
+# https://docs.microsoft.com/en-us/dotnet/api/system.io.compression.zipfileextensions.createentryfromfile?view=netframework-4.5
+# cannot create entry from directory - making overall process a slow one indeed
 
 # based on: https://www.cyberforum.ru/powershell/thread2023064.html#post10675062
 # see also: https://powershell.org/forums/topic/splitting-path-using-powershell/
 Add-Type -assembly 'System.IO.Compression'
-
 Add-Type -assembly 'System.IO.Compression.FileSystem'
+#
 $archivePath = "${env:USERPROFILE}\${name}.zip"
 if (test-path  -path $archivePath){
   remove-item -path $archivePath -force
 }
-
+# https://docs.microsoft.com/en-us/dotnet/api/system.io.compression.zipfile.createfromdirectory?view=netframework-4.8#System_IO_Compression_ZipFile_CreateFromDirectory_System_String_System_String_System_IO_Compression_CompressionLevel_System_Boolean_
 if ($debug){
-  write-output ('CreateFromDirectory({0},{1},[System.IO.Compression.CompressionLevel]::Optimal,$true)' -f $appDir, $archivePath )
+  write-output ('CreateFromDirectory("{0}", "{1}", [System.IO.Compression.CompressionLevel]::Optimal,$true)' -f $appDir, $archivePath )
 }
-[System.IO.Compression.ZipFile]::CreateFromDirectory($appDir,$archivePath,[System.IO.Compression.CompressionLevel]::Optimal,$true) | out-null
+[System.IO.Compression.ZipFile]::CreateFromDirectory($appDir, $archivePath, [System.IO.Compression.CompressionLevel]::Optimal,$true) | out-null
+
 
 [System.IO.Compression.ZipArchive]$ZipFile = [System.IO.Compression.ZipFile]::Open($archivePath, ([System.IO.Compression.ZipArchiveMode]::Update))
 $ZipFile.Dispose()
@@ -31,7 +37,8 @@ $ZipFile.Dispose()
 
 [System.IO.Compression.ZipArchive]$ZipFile = [System.IO.Compression.ZipFile]::Open($archivePath, ([System.IO.Compression.ZipArchiveMode]::Update))
 
-$len = $basedir.length
+$len = $basedir.length - 2
+# for C:
 
 get-childitem -path $resourceDir -recurse -file |
 foreach-object {
@@ -44,7 +51,9 @@ foreach-object {
   if ($debug){
     write-output ('long path (noqualifier) : {0}' -f $longPath_noqualifier)
   }
-  $shortpath = $longPath_noqualifier.Substring($len + 2);
+  $shortpath = $longPath_noqualifier.Substring($len + 1);
+	# for trailing "/"
+
   if ($debug){
     write-output ('short path : {0}' -f $shortpath)
   }
@@ -65,9 +74,9 @@ foreach-object {
     write-output ('rel path (2): {0}' -f $relpath2)
   }
   if ($debug){
-    write-output ('CreateEntryFromFile({0},{1},{2})' -f $ZipFile, $longPath,$relpath2 )
+    write-output ('CreateEntryFromFile({0},{1},{2})' -f $ZipFile, $longPath, $shortpath )
   }
-  [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($ZipFile, $longPath,$relpath2 )  | out-null
+  [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($ZipFile, $longPath, $shortpath ) | out-null
 }
 
 $zipFile.Dispose()
